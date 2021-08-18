6e0432bef62f54f8cd60bdce191d1a5315a7a023
machitake <glory.tm5@gmail.com>
2021-08-18 23:37:29 +0900

    実績追加（スライダー）

---
+++ js/script.js
@@ -0,0 +1,18 @@
+  //スライダー
+
+  $('.slider').slick({
+    // autoplay:true,
+    // autoplaySpeed:5000,
+    dots:true,
+    slidesToShow:4, //表示するスライドの数
+    //slidesToScroll:2, //１度にスクロールする数
+
+    responsive:[
+      {
+          breakpoint: 480,
+          settings:{
+              slidesToShow:1,
+          }
+      },
+  ]
+});
