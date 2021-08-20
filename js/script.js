9b6d13465f3a4a4d17531abddf205dafeb4ea3d4
machitake <glory.tm5@gmail.com>
2021-08-20 23:24:29 +0900

    完成（修正確認中）

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
   //スライダー
 
   $('.slider').slick({
-    // autoplay:true,
-    // autoplaySpeed:5000,
+    autoplay:true,
+    autoplaySpeed:800,
     dots:true,
     slidesToShow:4, //表示するスライドの数
     //slidesToScroll:2, //１度にスクロールする数
